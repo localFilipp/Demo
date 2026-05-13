e14485651dc9a7ee29f2e275703fc64887c553ee
37253 <mirjamtali@gmail.com>
2026-05-13 21:24:36 +0300

    portal

--- setup.py
+++ setup.py
@@ -144,6 +144,38 @@ class Block(Object):
         self.image.blit(block,(0, 0))
         self.mask = pygame.mask.from_surface(self.image)
 
+class Fire(Object):
+    ANIMATION_DELAY = 3
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height, "fire")
+        self.fire = load_sprite_sheets("Traps", "Fire", width, height)
+        self.image = self.fire["off"][0]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.animation_count = 0
+        self.animation_name = "off"
+
+    def on(self):
+        self.animation_name = "on"
+
+    def loop(self):
+        sprites = self.fire[self.animation_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
+
+class Portal(Object):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height, "portal")
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.image.fill((0, 0, 255))
+        self.mask = pygame.mask.from_surface(self.image)
+
 def get_background(name):
     image = pygame.image.load(join("assets", "background", name))
     image = pygame.transform.scale(image, (100, 100))
@@ -157,13 +189,14 @@ def get_background(name):
 
     return tiles, image
 
-def draw(window, background, bg_image, player, objects, offset_x):
+def draw(window, background, bg_image, player, objects, portal, offset_x):
     for tile in background:
         window.blit(bg_image, tile)
 
     for obj in objects:
         obj.draw(window, offset_x)
 
+    portal.draw(window, offset_x)
     player.draw(window, offset_x)
 
     pygame.display.update()
@@ -216,13 +249,19 @@ def main(window):
     block_size = 64
 
     player = Player(100,100, 50, 50)
+    fire = Fire(100, HEIGHT - block_size - 64, 16, 32)
+    fire.on()
+
+    portal = Portal(800, HEIGHT - block_size - 64, 40, 64)
+
     floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
-    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), Block(block_size * 3, HEIGHT - block_size * 4, block_size)]
+    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire]
 
     offset_x = 0
     scroll_area_width = 200
 
     run = True
+    won = False
     while run:
         clock.tick(FPS)
 
@@ -235,8 +274,20 @@ def main(window):
                     player.jump()
 
         player.loop(FPS)
+        fire.loop()
         handle_move(player, objects)
-        draw(window, background, bg_image, player, objects, offset_x)
+
+        if pygame.sprite.collide_mask(player, portal):
+            print("MÄNG VÕIDETUD!")
+            run = False
+
+        draw(window, background, bg_image, player, objects, portal, offset_x)
+
+        if won:
+            font = pygame.font.SysFont("arial", 60)
+            text = font.render("MÄNG VÕIDETUD!", True, (255, 255, 255))
+            window.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2))
+            pygame.display.update()
 
         if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or ((player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
             offset_x += player.x_vel
